dc960ea8f4191995de29fece647500b1685fef33
matiasalvarellos <matias.alvarellos@gmail.com>
2020-11-13 15:26:34 -0300

    cambios

--- routes/producto.js
+++ routes/producto.js
@@ -5,8 +5,10 @@ const controller= require("../controller/controllerProducto")
 router.get("/", controller.inicio);
 router.get("/crear",controller.formulario);
 router.post("/crear", controller.crear);
+
 router.get("/edit/:id", controller.edicion);
-router.post("/edit/:id", controller.update);
+router.put("/edit/:id", controller.update);
+
 router.get("/delete/:id", controller.destroy);
 
 module.exports=router
